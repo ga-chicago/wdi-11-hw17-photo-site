ecc84414593d6dfee059029159af667b3b9b1e5b
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-13 18:21:31 -0500

    created all files, installed all npm packages, set up server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const expressLayouts = require('express-ejs-layouts');
+app.set('view engine', 'ejs');
+
+// call the database
+require('./db/db');
+
+// enable css files
+app.use(express.static('public'))
+
+// controllers
+const usersController = require('./controllers/userController');
+const photosController = require('./controllers/photoController');
+
+// middle ware
+app.use(methodOverride("_method"));
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/users', usersController);
+app.use('/photos', photosController);
+
+// seeding data -- adding some data when you start development
+app.get('/seed', (req, res) => {
+			// Article.create();
+			// Author.create();
+
+
+
+	res.send('I just added some data for you');
+	
+})
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+	console.log("server is running on PORT " + PORT);
+})
