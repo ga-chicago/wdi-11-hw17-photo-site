06c67db66686c4ce3bfc2caec36efcd13c45af24
Zoe <zoereagan@gmail.com>
2018-04-15 18:02:40 -0500

    set up index route, index view

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../models/users');
 
-//
-// router.get('/', (req, res) => {
-// res.send("Hello!")
-//   });
+//INDEX route
+router.get('/', (req, res) => {
+  Users.find((err, foundUser) => {
+    if(err) console.log(err);
+    console.log(foundUser);
+    res.render('users/index.ejs', {users : foundUser});
+  });
+});
 
 
 
