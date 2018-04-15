e4a02fa3971eab25dbea5e72b20e459c2637550c
Hannah Werman <hannah.werman@gmail.com>
2018-04-15 13:38:03 -0500

    created post route for new user, modified index page to display users correctly

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -16,4 +16,15 @@ router.get('/new', (req, res) => {
 	res.render('users/new.ejs')	
 });
 
+router.post('/', (req, res) => {
+	Users.create({
+		username: req.body.username,
+		password: req.body.password
+	}, (err, newUser) => {
+		if(err) console.log(err);
+		else console.log(newUser);
+	});
+	res.redirect('/users')		
+})
+
 module.exports = router;
