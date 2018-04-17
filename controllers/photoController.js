b9c10a3ae873d60680dabeba03b9c5bc3cb4a612
marietrull <marietrull@gmail.com>
2018-04-17 12:40:06 -0500

    Commit 7: Updated photoController using async syntax

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -4,93 +4,117 @@ const Photos = require('../models/photos');
 const User = require('../models/users');
 
 //ROUTE TO PHOTO INDEX
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 
-	Photos.find((err, foundPhotos) => {
-		if(err)console.log(err);
-		res.render('photos/index.ejs',{photos: foundPhotos});
-	});
+	try {
+
+		const foundPhotos = await Photos.find();
+		res.render('photos/index.ejs',
+		{
+			photos: foundPhotos
+		})
+	} catch (err) {
+		res.send(err)
+	}
 });
 
 //ROUTE FOR LIST OF USERS
-router.get('/new', (req, res) => {
-	User.find({}, (err, allUsers) => {
-		res.render('photos/new.ejs', {
+router.get('/new', async (req, res) => {
+
+	try {
+		const allUsers = await User.find({})
+		res.render('photos/new.ejs',{
 			users: allUsers
-		});
-	});
+		})
+	} catch (err) {
+		res.send(err)
+	}
 });
 
 
 //ROUTE TO SHOW PAGE
 
-router.get('/:id', (req, res) => {
-	// console.log("---------------------------hittheshowroute--------------------------")
-	Photos.findById(req.params.id, (err, foundPhoto) => {
-		User.findOne({'articles._id': req.paramsid}, (err, foundUser) => {
-			if(err) console.log(err);
-			res.render('photos/show.ejs', {
-				photo: foundPhoto,
-				user: foundUser
-			});
-		});
-	});
+router.get('/:id', async (req, res) => {
+	try {
+		const foundPhoto = await Photos.findById(req.params.id);
+		const foundUser = await User.findOne({'articles._id': req.paramsid})
+
+		res.render('photos/show.ejs', {
+			photo: foundPhoto,
+			user: foundUser
+		})
+	} catch (err) {
+		res.send(err)
+	}
 });
 
 
-
 //ROUTE TO ADD NEW PHOTO
-router.post('/', (req, res) => {
-	User.findById(req.body.userId, (err, foundUser) => {
-		Photos.create(req.body, (err, addedPhoto) => {
-			if(err) console.log(err);
-			foundUser.photos.push(addedPhoto);
-			foundUser.save((err, data) => {
-				res.redirect('/photos');
-			})
+router.post('/', async (req, res) => {
+
+	try {
+
+		const foundUser = await User.findById(req.body.userId);
+		const addedPhoto = await Photos.create(req.body);
+
+		foundUser.photos.push(addedPhoto);
+		foundUser.save((err, data) => {
+			res.redirect('/photos')
 		})
-	});
+	} catch (err) {
+		res.send(err)
+	}
 });
 
 //ROUTE TO DELETE PHOTO
 
-router.delete('/:id', (req, res) => {
-	Photos.findByIdAndRemove(req.params.id, () => {
-		User.findOne({'photos._id': req.params.id}, (err, foundUser) => {
-			foundUser.photos.id(req.params.id).remove();
-			foundUser.save((err, data) => {
-				res.redirect('/photos');
-			});
+router.delete('/:id', async(req, res) => {
+
+	try {
+
+		const deletedPhoto = await Photos.findByIdAndRemove(req.params.id);
+		const foundUser = await User.findOne({'photos._id': req.params.id});
+
+		foundUser.photos.id(req.params.id).remove();
+		foundUser.save((err, data) => {
+			res.redirect('/photos');
 		});
-	});
+
+	} catch (err) {
+		res.send(err)
+	}
 });
 
 //ROUTE TO EDIT IMAGE
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', async(req, res) => {
 
-	Photos.findById(req.params.id, (err, foundPhoto) => {
-		User.find({}, (err, allUsers) => {
-			if (err) console.log(err);
-			User.findOne({'photos._id': req.params.index}, (err, foundUser) => {
-			res.render('photos/edit.ejs', {
-				photo: foundPhoto,
-				title: foundPhoto.title,
-				webLink: foundPhoto.webLink,
-				users: allUsers,
-				photographer: foundUser
-				});
-			});
-		});
-	});
+	try {
+		const foundPhoto = await Photos.findById(req.params.id);
+		const allUsers = await User.find({});
+		const foundUser = await User.findOne({'photos._id': req.params.index});
+
+		res.render('photos/edit.ejs', {
+			photo: foundPhoto,
+			title: foundPhoto.title,
+			webLink: foundPhoto.webLink,
+			users: allUsers,
+			photographer: foundUser
+		})
+	} catch (err) {
+		res.send(err)
+	}
 });
 
-//PUT ROUTE FOR AFTER EDIT
-router.put('/:id', (req, res) => {
 
-	Photos.findByIdAndUpdate(req.params.id, req.body, (err, updatedPhoto) => {
-		if(err) console.log(err);
+//PUT ROUTE FOR AFTER EDIT
+router.put('/:id', async (req, res) => {
+
+	try {
+		const updatedPhoto = await Photos.findByIdAndUpdate(req.params.id, req.body);
 		res.redirect('/photos')
-	});
+	} catch (err){
+		res.send(err)
+	}
 });
 
 
