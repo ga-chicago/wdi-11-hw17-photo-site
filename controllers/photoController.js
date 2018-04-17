cf41a8a92e7c77f69db2c98076bb0481a19e367d
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-17 12:42:53 -0500

    added next function to all routes in user and photo controllers

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -4,45 +4,45 @@ const Photos = require('../models/photos');
 const Users = require('../models/users');
 
 // index route
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
 	try {
 		const foundPhotos = await Photos.find();
 		res.render('photos/index.ejs', {
 			photos: foundPhotos
 		});		
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
 // new route
-router.get("/new", (req, res) => {
+router.get("/new", (req, res, next) => {
 	res.render('photos/new.ejs')
 })
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
 	try {
 		const createdPhoto = await Photos.create(req.body);
 		res.redirect("/photos");
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
 // show route
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
 	try {
 		const foundPhoto = await Photos.findById(req.params.id);
 		res.render('photos/show.ejs', {
 			photo: foundPhoto
 		})		
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
 // edit route
-router.get("/:id/edit", async (req, res) => {
+router.get("/:id/edit", async (req, res, next) => {
 	try {
 		const foundPhoto = await Photos.findById(req.params.id);
 		res.render("photos/edit.ejs", {
@@ -50,25 +50,25 @@ router.get("/:id/edit", async (req, res) => {
 			index: foundPhoto.id
 		});		
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", async (req, res, next) => {
 	try {
 		const updatedPhoto = Photos.findByIdAndUpdate(req.params.id, req.body);
 		res.redirect("/photos");
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req, res, next) => {
 	try {
 		const deletedPhoto = Photos.findByIdAndRemove(req.params.id);
 		res.redirect("/photos");
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,45 +4,45 @@ const Users = require('../models/users');
 const Photos = require('../models/photos');
 
 // index route
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
 	try {
 		const foundUsers = await Users.find();
 		res.render('users/index.ejs', {
 			users: foundUsers
 		})
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
 // new route
-router.get("/new", (req, res) => {
+router.get("/new", (req, res, next) => {
 	res.render('users/new.ejs')
 })
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
 	try {
 		const createdUser = await Users.create(req.body);
 		res.redirect("/users");
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
 // show route
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
 	try {
 		const foundUser = await Users.findById(req.params.id);
 		res.render('users/show.ejs', {
 			user: foundUser
 		})		
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
 // edit route
-router.get("/:id/edit", async (req, res) => {
+router.get("/:id/edit", async (req, res, next) => {
 	try {
 		const foundUser = await Users.findById(req.params.id);
 		res.render("users/edit.ejs", {
@@ -50,25 +50,25 @@ router.get("/:id/edit", async (req, res) => {
 			index: foundUser.id
 		});
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", async (req, res, next) => {
 	try {
 		const updatedUser = await Users.findByIdAndUpdate(req.params.id, req.body);
 		res.redirect("/users");
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req, res, next) => {
 	try {
 		const deletedUser = await Users.findByIdAndRemove(req.params.id);
 		res.redirect("/users");
 	} catch (err) {
-		res.send(err);
+		next(err);
 	}
 })
 
