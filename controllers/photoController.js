eed86747e1947b78194a1caf6c676137551f5a8b
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-17 12:30:59 -0500

    added async functions to photo controller

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -4,14 +4,15 @@ const Photos = require('../models/photos');
 const Users = require('../models/users');
 
 // index route
-router.get("/", (req, res) => {
-	Photos.find((err, foundPhotos) => {
-		if (err) console.log(err);
-		console.log(foundPhotos);
+router.get("/", async (req, res) => {
+	try {
+		const foundPhotos = await Photos.find();
 		res.render('photos/index.ejs', {
 			photos: foundPhotos
-		});
-	})
+		});		
+	} catch (err) {
+		res.send(err);
+	}
 })
 
 // new route
@@ -19,56 +20,56 @@ router.get("/new", (req, res) => {
 	res.render('photos/new.ejs')
 })
 
-router.post("/", (req, res) => {
-	Photos.create(req.body, (err, createdPhoto) => {
-		if (err) console.log(err);
-		console.log(createdPhoto);
+router.post("/", async (req, res) => {
+	try {
+		const createdPhoto = await Photos.create(req.body);
 		res.redirect("/photos");
-	})
+	} catch (err) {
+		res.send(err);
+	}
 })
 
 // show route
-router.get("/:id", (req, res) => {
-	Photos.findById(req.params.id, (err, foundPhoto) => {
-		if (err) console.log(err);
+router.get("/:id", async (req, res) => {
+	try {
+		const foundPhoto = await Photos.findById(req.params.id);
 		res.render('photos/show.ejs', {
 			photo: foundPhoto
-		})
-	})
+		})		
+	} catch (err) {
+		res.send(err);
+	}
 })
 
 // edit route
-router.get("/:index/edit", (req, res) => {
-	Photos.findById(req.params.index, (err, photo) => {
-			if (err) console.log(err);
-			console.log(photo);
-			res.render("photos/edit.ejs", {
-				photo: photo,
-				index: photo.id
-			});
-		})
+router.get("/:id/edit", async (req, res) => {
+	try {
+		const foundPhoto = await Photos.findById(req.params.id);
+		res.render("photos/edit.ejs", {
+			photo: foundPhoto,
+			index: foundPhoto.id
+		});		
+	} catch (err) {
+		res.send(err);
+	}
 })
 
-router.put("/:id", (req, res) => {
-	Photos.findByIdAndUpdate(
-		req.params.id,
-		{
-			username: req.body.username,
-			url: req.body.url,
-			caption: req.body.caption,
-			dateAdded: req.body.dateAdded
-		},
-		(err, photos) => {
-			if (err) console.log(err);
-			res.redirect("/photos");
-		})
-})
-
-router.delete("/:id", (req, res) => {
-	Photos.findByIdAndRemove(req.params.id, (err, deletedPhoto) => {
-		if (err) console.log(err);
+router.put("/:id", async (req, res) => {
+	try {
+		const updatedPhoto = Photos.findByIdAndUpdate(req.params.id, req.body);
 		res.redirect("/photos");
-	})
+	} catch (err) {
+		res.send(err);
+	}
+})
+
+router.delete("/:id", async (req, res) => {
+	try {
+		const deletedPhoto = Photos.findByIdAndRemove(req.params.id);
+		res.redirect("/photos");
+	} catch (err) {
+		res.send(err);
+	}
 })
 
 
