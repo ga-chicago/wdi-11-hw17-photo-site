1094eed4ddf55c89846d274439ca1f10761b615c
Hannah Werman <hannah.werman@gmail.com>
2018-04-16 22:43:28 -0500

    made photo show page, styled show and index pages

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Photos = require('../models/photos');
+const Users = require('../models/users')
 
 // ** index ** get route
 router.get('/', (req, res) => {
@@ -12,5 +13,35 @@ router.get('/', (req, res) => {
 		});	
 	});		
 });
+
+// ** new ** route
+router.get('/new', (req, res) => {
+	res.render('photos/new.ejs')		
+})
+
+router.post('/', (req, res) => {
+	Users.find({},(err, allUsers) => {
+		for (let i = 0; i < allUsers.length; i++) {
+			if (req.body.username === allUsers[i].username) {
+				Photos.create(req.body, (err, newPhoto) => {
+					if(err) console.log(err);
+					res.redirect('/photos/')
+				})
+			} else {
+				console.log("No matching username found. Please create a new user account") // this will eventually need to be a modal or another view but this is good enough for now
+			}
+		}	
+	})
+})
+
+// ** show ** route
+router.get('/:id', (req, res) => {
+	Photos.findById(req.params.id, (err, photo) => {
+		if(err) console.log(err);
+		res.render('photos/show.ejs', {
+			photo: photo
+		})	
+	})		
+})
 
 module.exports = router;
