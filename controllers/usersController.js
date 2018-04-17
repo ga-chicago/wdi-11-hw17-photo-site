a6812243d567ae3a8f8d7020b27a7c49d113c8a3
Zoe <zoereagan@gmail.com>
2018-04-17 12:33:31 -0500

    updated users SHOW route to async

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -36,14 +36,17 @@ router.post('/', async (req, res) => {
 
 
 //SHOW
-router.get('/:id', (req, res) => {
-  Users.findById(req.params.id, (err, foundUser) => {
-    if(err) console.log(err);
+router.get('/:id', async (req, res) => {
+  try {
+    const foundUser = await Users.findById(req.params.id);
     res.render('Users/show.ejs', {
       users: foundUser
     });
-  });
+  } catch(err) {
+    res.send(err)
+  }
 });
+
 
 
 //UPDATE
